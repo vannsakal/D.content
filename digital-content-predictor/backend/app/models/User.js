c8ba8924306ae5266151725c33cf920c8197d638
SUON Vannsakal <vannsakal.suon@gmail.com>
2026-09-20 00:29:18 +0700

    changed

--- digital-content-predictor/backend/app/models/User.js
+++ digital-content-predictor/backend/app/models/User.js
@@ -1,42 +1,41 @@
 const db = require('../config/db');
 
 class User {
-    static async createUser(data){
-        const [result] = await db.query(
-            "INSERT INTO users (email, password_hash, first_name, last_name) VALUES (?,?,?,?)",
+    static async createUser(data) {
+        const result = await db.query(
+            "INSERT INTO users (email, password_hash, first_name, last_name) VALUES ($1,$2,$3,$4) RETURNING user_id",
             [data.email, data.passwordHash, data.firstName, data.lastName]
         );
-        return result.insertId;
+        return result.rows[0].user_id;
     }
 
     static async findByEmail(email) {
-        const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
-        return rows[0] || null;
+        const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+        return result.rows[0] || null;
     }
 
     static async findById(userId) {
-    const [rows] = await db.query(
-      `SELECT u.user_id, u.email, u.first_name, u.last_name
-       FROM users u
-       WHERE u.user_id = ?`,
-      [userId]
-    );
-    return rows[0] || null;
-  }
-    static async updatePassword(userId, newPassword) {
-    await db.query(
-      'UPDATE users SET password_hash = ? WHERE user_id = ?',
-      [newPassword, userId]
-    );
-  }
+        const result = await db.query(
+            `SELECT user_id, email, first_name, last_name FROM users WHERE user_id = $1`,
+            [userId]
+        );
+        return result.rows[0] || null;
+    }
 
-  static async getPasswordById(userId) {
-    const [rows] = await db.query(
-      'SELECT password_hash FROM users WHERE user_id = ?',
-      [userId]
-    );
-    return rows[0]?.password_hash ?? null;
-  }
+    static async updatePassword(userId, newPassword) {
+        await db.query(
+            'UPDATE users SET password_hash = $1 WHERE user_id = $2',
+            [newPassword, userId]
+        );
+    }
+
+    static async getPasswordById(userId) {
+        const result = await db.query(
+            'SELECT password_hash FROM users WHERE user_id = $1',
+            [userId]
+        );
+        return result.rows[0]?.password_hash ?? null;
+    }
 }
 
 module.exports = User;
