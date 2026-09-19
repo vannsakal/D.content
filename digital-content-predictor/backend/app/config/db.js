2c6bd748f5556985ab654aec55167092e4f4e820
SUON Vannsakal <vannsakal.suon@gmail.com>
2026-09-20 01:21:52 +0700

    update

--- digital-content-predictor/backend/app/config/db.js
+++ digital-content-predictor/backend/app/config/db.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
+const dns = require('dns');
 require('dotenv').config();
+
+// Force IPv4
+dns.setDefaultResultOrder('ipv4first');
 
 const pool = new Pool({
   host: process.env.DB_HOST,
